fb33e1102db0824017e2a6f1461f6c0da084f103
Brnadon Stephens <brandonstephens@me.com>
2022-09-22 21:26:39 -0400

    another exercise

--- ex8/ex8.js
+++ ex8/ex8.js
@@ -1,10 +1,30 @@
-$(document).ready(function(){
-	var $btn = $("#btn"),
-		$list = $("#list");
+$(document).ready(function () {
+  // creation
+  var $btn = $("#btn"),
+    $list = $("#list"),
+    clicks = ASQ.react.of(),
+    messages = ASQ.react.of(),
+    latest;
 
-	$btn.click(function(evt){
-		// TODO
-	});
+  $btn.click(function (event) {
+    clicks.push(event);
+  });
 
-	// TODO: setup sampled sequence, populate $list
+  // -----------------------------------------------------------------------------
+
+  // consumption
+  setInterval(() => {
+    if (latest) {
+      messages.push("clicked");
+      latest = null;
+    }
+  }, 1000);
+
+  clicks.val((event) => {
+    latest = event;
+  });
+
+  messages.val((message) => {
+    $list.append($(`<div>${message}</div>`));
+  });
 });
